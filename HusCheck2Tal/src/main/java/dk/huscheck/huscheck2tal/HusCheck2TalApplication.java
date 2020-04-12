2ece6abe4b648785a611f98e4297506d4e528c88
jo <joergen.overgaard@gmail.com>
2020-04-12 23:17:37 +0200

    Google login

--- HusCheck2Tal/src/main/java/dk/huscheck/huscheck2tal/HusCheck2TalApplication.java
+++ HusCheck2Tal/src/main/java/dk/huscheck/huscheck2tal/HusCheck2TalApplication.java
@@ -3,7 +3,6 @@ package dk.huscheck.huscheck2tal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//TODO: test test
 @SpringBootApplication
 public class HusCheck2TalApplication {
 
