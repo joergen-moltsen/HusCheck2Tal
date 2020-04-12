a3d0744bb86ff1feb1dba3f0e91e2b86b3acea54
jo <joergen.overgaard@gmail.com>
2020-04-12 11:27:02 +0200

    nothing

--- HusCheck2Tal/src/main/java/dk/huscheck/huscheck2tal/HusCheck2TalApplication.java
+++ HusCheck2Tal/src/main/java/dk/huscheck/huscheck2tal/HusCheck2TalApplication.java
@@ -3,6 +3,7 @@ package dk.huscheck.huscheck2tal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//TODO: 
 @SpringBootApplication
 public class HusCheck2TalApplication {
 
