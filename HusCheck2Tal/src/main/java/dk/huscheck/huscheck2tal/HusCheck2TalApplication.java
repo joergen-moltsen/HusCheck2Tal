454f3e158c6cfd142ab48165da17c1998b2ad1f5
jo <joergen.overgaard@gmail.com>
2020-04-12 11:07:09 +0200

    initial commit

---
+++ HusCheck2Tal/src/main/java/dk/huscheck/huscheck2tal/HusCheck2TalApplication.java
@@ -0,0 +1,13 @@
+package dk.huscheck.huscheck2tal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HusCheck2TalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HusCheck2TalApplication.class, args);
+	}
+
+}
---
+++ HusCheck2Tal/src/test/java/dk/huscheck/huscheck2tal/HusCheck2TalApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.huscheck.huscheck2tal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HusCheck2TalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
