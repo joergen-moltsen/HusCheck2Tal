a92d8327756f3287123bcaa3de944c33665f9663
jo <joergen.overgaard@gmail.com>
2020-04-24 22:42:33 +0200

    error handling

--- HusCheck2Tal/src/main/java/dk/huscheck/huscheck2tal/security/HttpSecurityConfig.java
+++ HusCheck2Tal/src/main/java/dk/huscheck/huscheck2tal/security/HttpSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
@@ -14,6 +15,8 @@ public class HttpSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
+		
 		// @formatter:off
 		http
 			.authorizeRequests(a -> a
@@ -29,7 +32,12 @@ public class HttpSecurityConfig extends WebSecurityConfigurerAdapter {
 			.exceptionHandling(e -> e
 				.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
 			)
-			.oauth2Login();
+			.oauth2Login(o -> o
+		            .failureHandler((request, response, exception) -> {
+					    request.getSession().setAttribute("error.message", exception.getMessage());
+					    handler.onAuthenticationFailure(request, response, exception);
+		            })
+		        );
 		// @formatter:on
 	}
 }
